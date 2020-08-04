0816f920811f08bbae20f006b6fd22ae457bb00d
rodrigo novais <roo.novais@gmail.com>
2020-08-04 15:27:31 -0300

    web: Global styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Landing from './pages/Landing';
 
-function App() {
+import './assets/styles/global.css';
+
+const App: React.FC = () => {
     return (
-        <div className="container">
-
-        </div>
+        <Landing />
     );
 }
 
