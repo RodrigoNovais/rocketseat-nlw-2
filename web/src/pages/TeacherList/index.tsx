0f20b1de46ae48e50d3a84fc934f660c3f51db2b
rodrigo novais <roo.novais@gmail.com>
2020-08-04 20:47:07 -0300

    web: Teacher List Page

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
 
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
+
 import './styles.css';
 
 const TeacherList: React.FC = () => {
     return (
-        <div>
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis.">
+                <form id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Matéria</label>
+                        <input type="text" name="subject" id="subject"/>
+                    </div>
 
+                    <div className="input-block">
+                        <label htmlFor="week-day">Dia da Semana</label>
+                        <input type="text" name="week-day" id="week-day"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="time">Hora</label>
+                        <input type="text" name="time" id="time"/>
+                    </div>
+                </form>
+            </PageHeader>
+
+            <main>
+                <TeacherItem />
+            </main>
         </div>
     );
 };
