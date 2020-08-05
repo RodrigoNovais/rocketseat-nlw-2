d6bfe8fc85673ee9a066a9ea2c23582b1c74114a
rodrigo novais <roo.novais@gmail.com>
2020-08-05 20:03:51 -0300

    server: Configure database connection

---
+++ server/src/database/migrations/03_connections.ts
@@ -0,0 +1,22 @@
+import Knex from 'knex'
+
+export const up = async (knex: Knex) => {
+    return knex.schema.createTable('connections', table => {
+        table.increments('id').primary()
+
+        table.integer('user_id')
+            .references('id')
+            .inTable('users')
+            .notNullable()
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+
+        table.timestamp('created_at')
+            .defaultTo('now()')
+            .notNullable()
+    })
+}
+
+export const down = async (knex: Knex) => {
+    return knex.schema.dropTable('connections')
+}
