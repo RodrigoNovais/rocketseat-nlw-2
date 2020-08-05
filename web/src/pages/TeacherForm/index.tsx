c3f7f45e14124aefd9b95e6ba34d85231dfd6436
rodrigo novais <roo.novais@gmail.com>
2020-08-05 17:24:52 -0300

    web: Teacher Form Page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,83 @@
 import React from 'react'
 
+import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import TextArea from '../../components/TextArea';
+import Select from '../../components/Select';
+
+import warningIcon from '../../assets/images/icons/warning.svg'
+
 import './styles.css';
 
 const TeacherForm: React.FC = () => {
     return (
-        <div>
+        <div id="page-teacher-form" className="container">
+            <PageHeader
+                title="Que incrível que você quer dar aulas."
+                description="O primeiro passo é preencher esse formulário de inscrição."/>
 
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <Input name="name" label="Nome Completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsApp" label="WhatsApp" />
+
+                    <TextArea name="bio" label="Biografia" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Select name="subject" label="Matéria" options={[
+                        { value: 'Artes', label: 'Artes' },
+                        { value: 'Biologia', label: 'Biologia' },
+                        { value: 'Ciências', label: 'Ciências' },
+                        { value: 'Educação Física', label: 'Educação Física' },
+                        { value: 'Geografia', label: 'Geografia' },
+                        { value: 'Historia', label: 'Historia' },
+                        { value: 'Matemática', label: 'Matemática' },
+                        { value: 'Português', label: 'Português' },
+                        { value: 'Química', label: 'Química' },
+                    ]} />
+
+                    <Input name="cost" label="Custo da sua hora por aula" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        Horários disponíveis
+                        <button type="button">+ Novo horário</button>
+                    </legend>
+
+                    <div className="schedule-item">
+                            <Select name="week-day" label="Dia da Semana"  options={[
+                                { value: '0', label: 'Domingo' },
+                                { value: '1', label: 'Segunda-Feira' },
+                                { value: '2', label: 'Terça-Feira' },
+                                { value: '3', label: 'Quarta-Feira' },
+                                { value: '4', label: 'Quinta-Feira' },
+                                { value: '5', label: 'Sexta-Feira' },
+                                { value: '6', label: 'Sábado' },
+                            ]} />
+
+                            <Input type="time" name="from" label="Das" />
+
+                            <Input type="time" name="to" label="Até" />
+                        </div>
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso Importante"/>
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+
+                    <button type="button">Salvar cadastro</button>
+                </footer>
+            </main>
         </div>
     );
 };
