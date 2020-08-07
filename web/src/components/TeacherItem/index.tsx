202e973a3ecd0269b67f5e807cba14b885b23b58
rodrigo novais <roo.novais@gmail.com>
2020-08-06 22:32:15 -0300

    web: Teacer Item Component props

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,36 +2,52 @@ import React from 'react'
 
 import whatsAppIcon from '../../assets/images/icons/whatsapp.svg'
 
-import './styles.css';
+import './styles.css'
+import api from '../../services/api'
 
-const TeacherItem: React.FC = () => {
+export type Teacher = {
+    id: number
+    subject: string
+    cost: number
+    name: string
+    avatar: string
+    whatsApp: string
+    bio: string
+}
+
+type TeacherItemProps = {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    const createNewConnection = () => {
+        api.post('/connections', { user_id: teacher.id })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/11841314?s=460" alt="Rodrigo Novais"/>
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Rodrigo Novais</strong>
-                    <span>Culinária</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Aquele famoso texto aleatório para teste de layout.<br /><br />
-                Qualquer informação recebida por este meio não é real, todo o conteúdo é inventado apenas para preencher o espaço vago e criar uma sensação de conteúdo integro para o teste a interface.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 20,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <a target="_blank" rel="noopener noreferrer" href={`https://wa.me/${0}`}>
+                <a target="_blank" rel="noopener noreferrer" href={`https://wa.me/${teacher.whatsApp}`} onClick={createNewConnection}>
                     <img src={whatsAppIcon} alt="WhatsApp"/>
                     Entrar em Contato
                 </a>
             </footer>
         </article>
-    );
-};
+    )
+}
 
-export default TeacherItem;
+export default TeacherItem
