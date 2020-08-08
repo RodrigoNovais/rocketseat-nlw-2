ddd414765da955f9bf5c8ec1e09e2bd5ae2b2aa5
rodrigo novais <roo.novais@gmail.com>
2020-08-08 18:50:03 -0300

    mobile: Register new connection

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -7,6 +7,8 @@ import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
 import whatsAppIcon from '../../assets/images/icons/whatsapp.png'
 
+import api from '../../services/api'
+
 import styles from './styles'
 
 export type Teacher = {
@@ -28,6 +30,8 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
     const [isFavorited, setIsFavorited] = useState<boolean>(favorited)
 
     const handleLinkToWhatsApp = () => {
+        api.post('/connections', { user_id: teacher.id })
+
         Linking.openURL(`whatsapp:send?phone=${teacher.whatsApp}`)
     }
 
