71b71b9f93e40000de0dea12279cebfcb4be15b7
rodrigo novais <roo.novais@gmail.com>
2020-08-08 16:33:31 -0300

    mobile: Teacher Item Component props

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Image, Text } from 'react-native'
+import { View, Image, Text, Linking } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
 
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
@@ -8,28 +8,44 @@ import whatsAppIcon from '../../assets/images/icons/whatsapp.png'
 
 import styles from './styles'
 
-const TeacherItem: React.FC = () => {
+export type Teacher = {
+    id: number
+    subject: string
+    cost: number
+    name: string
+    avatar: string
+    whatsApp: string
+    bio: string
+}
+
+type TeacherItemProps = {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    const handleLinkToWhatsApp = () => {
+        Linking.openURL(`whatsapp:send?phone=${teacher.whatsApp}`)
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
-                <Image style={styles.avatar} source={{ uri: '' }} />
+                <Image style={styles.avatar} source={{ uri: teacher.avatar }} />
 
                 <View style={styles.profileInfo}>
-                    <Text style={styles.name}>Diego Fernandes</Text>
-                    <Text style={styles.subject}>Química</Text>
+                    <Text style={styles.name}>{teacher.name}</Text>
+                    <Text style={styles.subject}>{teacher.subject}</Text>
                 </View>
             </View>
 
             <Text style={styles.bio}>
-                Entusiasta das melhores tecnologias de química avançada.
-                {'\n'}{'\n'}
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+                {teacher.bio}
             </Text>
 
             <View style={styles.footer}>
                 <Text style={styles.price}>
                     Preço/hora {'   '}
-                    <Text style={styles.priceValue}>R$ 20,00</Text>
+                    <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
                 </Text>
 
                 <View style={styles.buttonsContainer}>
@@ -38,7 +54,7 @@ const TeacherItem: React.FC = () => {
                         {/* <Image source={unfavoriteIcon} /> */}
                     </RectButton>
 
-                    <RectButton style={styles.contactButton}>
+                    <RectButton onPress={handleLinkToWhatsApp} style={styles.contactButton}>
                         <Image source={whatsAppIcon} />
                         <Text style={styles.contactButtonText}>Entrar em contato</Text>
                     </RectButton>
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -4,14 +4,33 @@ import { BorderlessButton, RectButton } from 'react-native-gesture-handler'
 import { Feather } from '@expo/vector-icons'
 
 import PageHeader from '../../components/PageHeader'
-import TeacherItem from '../../components/TeacherItem'
+import TeacherItem, { Teacher } from '../../components/TeacherItem'
+
+import api from '../../services/api'
 
 import styles from './style'
 
 const TeacherList: React.FC = () => {
     const [isFiltersVisible, setIsFiltersVisible] = useState<boolean>(false)
 
+    const [teachers, setTeachers] = useState<Teacher[]>([])
+
+    const [subject, setSubject] = useState<string>('')
+    const [weekDay, setWeekDay] = useState<number>(0)
+    const [time, setTime] = useState<string>('')
+
     const handleToggleFiltersVisible = () => setIsFiltersVisible(!isFiltersVisible)
+
+    const handleFiltersSubmit = async () => {
+        const response = await api.get('/classes', { params: {
+            subject,
+            week_day: weekDay,
+            time
+        }})
+
+        setIsFiltersVisible(false)
+        setTeachers(response.data)
+    }
 
     return (
         <View style={styles.container}>
@@ -23,21 +42,24 @@ const TeacherList: React.FC = () => {
                 { isFiltersVisible && (
                     <View style={styles.searchForm}>
                         <Text style={styles.label}>Matéria</Text>
-                        <TextInput style={styles.input} placeholder="Qual a matéria?" placeholderTextColor="#c1bccc" />
+                        <TextInput style={styles.input} placeholder="Qual a matéria?" placeholderTextColor="#c1bccc"
+                            value={subject} onChangeText={text => setSubject(text)} />
 
                         <View style={styles.inputGroup}>
                             <View style={styles.inputBlock}>
                                 <Text style={styles.label}>Dia da semana</Text>
-                                <TextInput style={styles.input} placeholder="Qual o dia?" placeholderTextColor="#c1bccc" />
+                                <TextInput style={styles.input} placeholder="Qual o dia?" placeholderTextColor="#c1bccc"
+                                    value={weekDay.toString()} onChangeText={text => setWeekDay(Number(text))} />
                             </View>
 
                             <View style={styles.inputBlock}>
                                 <Text style={styles.label}>Horário</Text>
-                                <TextInput style={styles.input} placeholder="Qual horário?" placeholderTextColor="#c1bccc" />
+                                <TextInput style={styles.input} placeholder="Qual horário?" placeholderTextColor="#c1bccc"
+                                    value={time} onChangeText={text => setTime(text)} />
                             </View>
                         </View>
 
-                        <RectButton style={styles.submitButton}>
+                        <RectButton onPress={handleFiltersSubmit} style={styles.submitButton}>
                             <Text style={styles.submitButtonText}>Filtrar</Text>
                         </RectButton>
                     </View>
@@ -48,7 +70,9 @@ const TeacherList: React.FC = () => {
                 paddingHorizontal: 16,
                 paddingBottom: 16,
             }}>
-                <TeacherItem />
+                { teachers.map(teacher => (
+                    <TeacherItem key={teacher.id} teacher={teacher} />
+                ))}
             </ScrollView>
         </View>
     )
