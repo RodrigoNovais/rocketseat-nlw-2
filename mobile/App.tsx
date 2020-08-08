36ee79459ebda39bd4e028813439c443622b377d
rodrigo novais <roo.novais@gmail.com>
2020-08-07 22:57:19 -0300

    mobile: Stack Navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,7 +7,7 @@ import { AppLoading } from 'expo'
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
 
-import Landing from './src/pages/Landing'
+import AppStack from './src/routes/AppStack'
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -22,7 +22,7 @@ export default function App() {
 
     return (
         <Fragment>
-            <Landing />
+            <AppStack />
             <StatusBar style="light" />
         </Fragment>
     )
