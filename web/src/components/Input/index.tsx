3123363f2e2bf7d9ae15bde78f359161f6f3e8b5
rodrigo novais <roo.novais@gmail.com>
2020-08-13 14:08:04 -0300

    web: Update Input component with label placeholder

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -3,15 +3,16 @@ import React, { InputHTMLAttributes } from 'react'
 import './styles.css'
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
-    label: string
+    label?: string
     name: string
+    stacked?: boolean
 }
 
-const Input: React.FC<InputProps> = ({ label, name, ...rest }) => {
+const Input: React.FC<InputProps> = ({ label, name, stacked, ...rest }) => {
     return (
-        <div className="input-block">
-            <label htmlFor={name}>{label}</label>
-            <input type="text" name={name} id={name} {...rest} />
+        <div className={`input-block ${stacked && 'input-stacked'}`}>
+            <input name={name} id={name} {...rest} />
+            { label && <label htmlFor={name}>{label}</label> }
         </div>
     )
 }
