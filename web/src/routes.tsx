e95fcc9f73bea8adc1f37373f31ecb9f7702e1cb
rodrigo novais <roo.novais@gmail.com>
2020-08-13 15:43:22 -0300

    web: Update routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -5,12 +5,20 @@ import Landing from './pages/Landing'
 import TeacherForm from './pages/TeacherForm'
 import TeacherList from './pages/TeacherList'
 
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ForgotPassword from './pages/ForgotPassword'
+
 const Routes: React.FC = () => (
     <BrowserRouter>
         <Switch>
             <Route path="/" exact component={Landing} />
             <Route path="/study" component={TeacherList} />
             <Route path="/give-classes" component={TeacherForm} />
+
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/forgot-password" component={ForgotPassword} />
 
             <Route path="*">
                 <Redirect to="/" />
